f1ca748292c18f413fb240b051af85515a48b27c
tesschung <geobera0910@naver.com>
2020-03-06 22:31:50 +0900

    HashMap<String, ArrayList<Pokemon>> pokemons = new HashMap<>()

--- JavaBasic/src/Pokemon.java
+++ JavaBasic/src/Pokemon.java
@@ -2,6 +2,7 @@ public class Pokemon {
     public final String name;
     public final int cp;
 
+    // 객체 생성
     public Pokemon(String name, int cp) {
         this.name = name;
         this.cp = cp;
@@ -9,6 +10,7 @@ public class Pokemon {
 
     @Override
     public String toString() {
+        // 객체를 문자열 형태로 바꿔서 String형태로 반환
         return name + "(" + cp + ")";
     }
 }
